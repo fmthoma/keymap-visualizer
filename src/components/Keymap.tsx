94788bd60b4bd12e26f141a9fb63cc70ddf723cb
Franz Thoma <franz.thoma@tngtech.com>
2025-03-15 12:11:48 +0100

    Fix warning

--- src/components/Keymap.tsx
+++ src/components/Keymap.tsx
@@ -8,10 +8,10 @@ export type KeymapProps = {
 
 const zipWith =
   <A, B>(as: A[], bs: B[]) =>
-  <C,>(f: (a: A, b: B) => C): C[] => {
+  <C,>(f: (a: A, b: B, index: number) => C): C[] => {
     const res = [];
     for (let i = 0; i < Math.min(as.length, bs.length); i++) {
-      res.push(f(as[i], bs[i]));
+      res.push(f(as[i], bs[i], i));
     }
     return res;
   };
@@ -79,8 +79,8 @@ export function Keymap(props: KeymapProps) {
   const keys = zipWith(
     props.matrix,
     props.keys,
-  )((loc, binding) => (
-    <Key loc={loc} binding={binding} mods={modifiers} down={false} />
+  )((loc, binding, index) => (
+    <Key loc={loc} binding={binding} mods={modifiers} down={false} key={index} />
   ));
 
   return <div style={{ position: 'relative' }}>{keys}</div>;
