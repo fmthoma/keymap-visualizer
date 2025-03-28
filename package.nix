a669bfe8ddd8778de1d7b59fac851adbb5efff33
Franz Thoma <franz.thoma@tngtech.com>
2025-03-28 13:24:27 +0100

    Include toggle script in nix package

--- package.nix
+++ package.nix
@@ -4,62 +4,74 @@
   buildNpmPackage,
   electron_34,
   makeWrapper,
+  writeShellScriptBin,
+  symlinkJoin,
 }:
 
 let
   electron = electron_34;
-in
 
-buildNpmPackage rec {
-  pname = "keymap-visualizer";
-  version = "0.1.0";
+  keymapVisualizerApp = buildNpmPackage rec {
+    pname = "keymap-visualizer";
+    version = "0.1.0";
 
-  src = ./.;
+    src = ./.;
 
-  npmDepsHash = "sha256-CdalY0l37Y0p1s/ohECm036VYj/teWE/yjL6vjxugK4=";
-  npmBuildScript = "build";
-  makeCacheWritable = true;
+    npmDepsHash = "sha256-CdalY0l37Y0p1s/ohECm036VYj/teWE/yjL6vjxugK4=";
+    npmBuildScript = "build";
+    makeCacheWritable = true;
 
-  nativeBuildInputs = [ makeWrapper ];
+    nativeBuildInputs = [ makeWrapper ];
 
-  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+    env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
-  postBuild = ''
-    # electronDist needs to be writable
-    cp -r ${electron.dist} electron-dist
-    chmod -R u+w electron-dist
+    postBuild = ''
+      # electronDist needs to be writable
+      cp -r ${electron.dist} electron-dist
+      chmod -R u+w electron-dist
 
-    npm exec electron-builder -- \
-        --publish never \
-        --dir \
-        -c.electronDist=electron-dist \
-        -c.electronVersion=${electron.version}
+      npm exec electron-builder -- \
+          --publish never \
+          --dir \
+          -c.electronDist=electron-dist \
+          -c.electronVersion=${electron.version}
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir --parents \
+        $out/bin \
+        $out/share
+
+      readonly dist=release/build/linux-unpacked
+
+      cp -a $dist/resources $out/share/${pname}
+
+      makeWrapper '${lib.getExe electron}' $out/bin/${pname}-app \
+        --set-default ELECTRON_IS_DEV 0 \
+        --set-default RESOURCES_PATH $out/share/${pname}/assets \
+        --add-flags $out/share/${pname}/app.asar \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Visualize my QMK keymaps";
+      homepage = "https://github.com/fmthoma/keymap-visualizer";
+      platforms = electron.meta.platforms;
+    };
+  };
+
+  wrapper = writeShellScriptBin "keymap-visualizer" ''
+    (echo -n "toggle" | nc -UN /tmp/keymap.sock) || exec ${keymapVisualizerApp}/bin/keymap-visualizer-app "$@"
   '';
 
-  installPhase = ''
-    runHook preInstall
-
-    mkdir --parents \
-      $out/bin \
-      $out/share
-
-    readonly dist=release/build/linux-unpacked
-
-    cp -a $dist/resources $out/share/${pname}
-
-    makeWrapper '${lib.getExe electron}' $out/bin/${pname} \
-      --set-default ELECTRON_IS_DEV 0 \
-      --set-default RESOURCES_PATH $out/share/${pname}/assets \
-      --add-flags $out/share/${pname}/app.asar \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
-
-    runHook postInstall
-  '';
-
-  meta = {
-    description = "Visualize my QMK keymaps";
+in symlinkJoin {
+  name = "keymap-visualizer";
+  paths = [ keymapVisualizerApp wrapper ];
+  meta = keymapVisualizerApp.meta // {
     mainProgram = "keymap-visualizer";
-    homepage = "https://github.com/fmthoma/keymap-visualizer";
-    platforms = electron.meta.platforms;
   };
 }
