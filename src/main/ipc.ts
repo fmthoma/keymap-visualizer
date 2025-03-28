05c9931418fc6104ce2aa59afdcf0ffc016e4f2b
Franz Thoma <franz.thoma@tngtech.com>
2025-03-28 13:35:54 +0100

    Remove ipc-example

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -11,11 +11,6 @@ import { handleKeyboardSwitch } from './tray';
 import { icons } from './resources';
 
 export function setupIpcHandlers() {
-  ipcMain.on('ipc-example', async (event, arg) => {
-    const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-    console.log(msgTemplate(arg));
-    event.reply('ipc-example', msgTemplate('pong'));
-  });
 
   ipcMain.on('screenshot', (_event, rect: Rectangle) => {
     const mainWindow = getMainWindow();
